53dc19b382e361d4d70ac2500f4ffe37ebafe1d8
robertdubson <dubson2001@gmail.com>
2023-04-18 14:05:38 +0300

    Created user in domain level model.

--- BusinessLogic/IUser.cs
+++ BusinessLogic/IUser.cs
@@ -6,5 +6,9 @@ namespace BusinessLogic
 {
     interface IUser
     {
+        public bool Login(string password, string username);
+        
+        public void Logout();
+
     }
 }
--- BusinessLogic/User.cs
+++ BusinessLogic/User.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BusinessLogic
 {
-    class User
+    class User : IUser
     {
+
+        public string UserName { get; private set; }
+
+        private string _passwordHash;
+
+        public string UserEmail { get; private set; }
+
+        public User(string password, string username) 
+        {
+            UserName = username;
+
+            _passwordHash = GetHashString(password);
+
+        }
+        
+
+        public bool Login(string password, string username)
+        {
+            return GetHashString(password) == _passwordHash & username == UserName;
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
     }
 }
